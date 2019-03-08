2df8edf3475e45fcebbc730a209f1b8f823dcfa1
xxxxue <1659809758@qq.com>
2019-03-08 17:03:10 +0800

    10. 组合模式

---
+++ 10.组合模式/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.组合模式
+{
+    //Program2 是 安全式的组合模式
+
+    internal class Program
+    {
+        // 通过一些简单图形以及一些复杂图形构建图形树来演示组合模式
+        // 客户端调用
+        private static void Main(string[] args)
+        {
+            ComplexGraphics complexGraphics = new ComplexGraphics("一个复杂图形和两条线段组成的复杂图形");
+            complexGraphics.Add(new Line("线段A"));
+
+            ComplexGraphics CompositeCG = new ComplexGraphics("一个圆和一条线组成的复杂图形");
+            CompositeCG.Add(new Circle("圆"));
+            CompositeCG.Add(new Circle("线段B"));
+
+            complexGraphics.Add(CompositeCG);
+            Line lineC = new Line("线段C");
+            complexGraphics.Add(lineC);
+
+            // 显示复杂图形的画法
+            Console.WriteLine("复杂图形的绘制如下：");
+            Console.WriteLine("---------------------");
+            complexGraphics.Draw();
+            Console.WriteLine("复杂图形绘制完成");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+
+            // 移除一个组件再显示复杂图形的画法
+            complexGraphics.Remove(lineC);
+            Console.WriteLine("移除线段C后，复杂图形的绘制如下：");
+            Console.WriteLine("---------------------");
+            complexGraphics.Draw();
+            Console.WriteLine("复杂图形绘制完成");
+            Console.WriteLine("---------------------");
+            Console.Read();
+
+
+
+            /*
+             
+             优点：
+
+                    组合模式使得客户端代码可以一致地处理对象和对象容器，无需关系处理的单个对象，还是组合的对象容器。
+                    将”客户代码与复杂的对象容器结构“解耦。
+                    可以更容易地往组合对象中加入新的构件。
+
+             缺点：
+                    使得设计更加复杂。客户端需要花更多时间理清类之间的层次关系。（这个是几乎所有设计模式所面临的问题）。
+                
+             注意的问题：
+                
+                    有时候系统需要遍历一个树枝结构的子构件很多次，这时候可以考虑把遍历子构件的结构存储在父构件里面作为缓存。
+                    客户端尽量不要直接调用树叶类中的方法（在我上面实现就是这样的，创建的是一个树枝的具体对象，应该使用Graphics complexGraphics = new ComplexGraphics("一个复杂图形和两条线段组成的复杂图形");），而是借用其父类（Graphics）的多态性完成调用，这样可以增加代码的复用性。
+             
+             */
+        }
+    }
+
+    /// <summary>
+    /// 图形抽象类，
+    /// </summary>
+    public abstract class Graphics
+    {
+        public string Name { get; set; }
+
+        public Graphics(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Draw();
+
+        public abstract void Add(Graphics g);
+
+        public abstract void Remove(Graphics g);
+    }
+
+    /// <summary>
+    /// 简单图形类——线
+    /// </summary>
+    public class Line : Graphics
+    {
+        public Line(string name)
+            : base(name)
+        { }
+
+        // 重写父类抽象方法
+        public override void Draw()
+        {
+            Console.WriteLine("画  " + Name);
+        }
+
+        // 因为简单图形在添加或移除其他图形，所以简单图形Add或Remove方法没有任何意义
+        // 如果客户端调用了简单图形的Add或Remove方法将会在运行时抛出异常
+        // 我们可以在客户端捕获该类移除并处理
+        public override void Add(Graphics g)
+        {
+            throw new Exception("不能向简单图形Line添加其他图形");
+        }
+
+        public override void Remove(Graphics g)
+        {
+            throw new Exception("不能向简单图形Line移除其他图形");
+        }
+    }
+
+    /// <summary>
+    /// 简单图形类——圆
+    /// </summary>
+    public class Circle : Graphics
+    {
+        public Circle(string name)
+            : base(name)
+        { }
+
+        // 重写父类抽象方法
+        public override void Draw()
+        {
+            Console.WriteLine("画  " + Name);
+        }
+
+        public override void Add(Graphics g)
+        {
+            throw new Exception("不能向简单图形Circle添加其他图形");
+        }
+
+        public override void Remove(Graphics g)
+        {
+            throw new Exception("不能向简单图形Circle移除其他图形");
+        }
+    }
+
+    /// <summary>
+    /// 复杂图形，由一些简单图形组成,这里假设该复杂图形由一个圆两条线组成的复杂图形
+    /// </summary>
+    public class ComplexGraphics : Graphics
+    {
+        private List<Graphics> complexGraphicsList = new List<Graphics>();
+
+        public ComplexGraphics(string name)
+            : base(name)
+        { }
+
+        /// <summary>
+        /// 复杂图形的画法
+        /// </summary>
+        public override void Draw()
+        {
+            foreach (Graphics g in complexGraphicsList)
+            {
+                g.Draw();
+            }
+        }
+
+        public override void Add(Graphics g)
+        {
+            complexGraphicsList.Add(g);
+        }
+
+        public override void Remove(Graphics g)
+        {
+            complexGraphicsList.Remove(g);
+        }
+    }
+}
