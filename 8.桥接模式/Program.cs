62ea8b6dcda8113dcf5d9614af9d4b0462f77c19
xxxxue <1659809758@qq.com>
2019-03-08 16:30:52 +0800

    8.  桥接模式

---
+++ 8.桥接模式/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace _8.桥接模式
+{
+    //Program2 在 三层架构中的应用
+
+    internal class Program
+    {        
+        private static void Main(string[] args)
+        {
+            // 创建一个遥控器
+            RemoteControl remoteControl = new ConcreteRemote();
+            // 长虹电视机
+            remoteControl.Implementor = new ChangHong();
+            remoteControl.On();
+            remoteControl.SetChannel();
+            remoteControl.Off();
+            Console.WriteLine();
+
+            // 三星牌电视机
+            remoteControl.Implementor = new Samsung();
+            remoteControl.On();
+            remoteControl.SetChannel();
+            remoteControl.Off();
+            Console.Read();
+
+
+            /*
+             优点：
+
+                把抽象接口与其实现解耦。                
+                抽象和实现可以独立扩展，不会影响到对方。                
+                实现细节对客户透明，对用于隐藏了具体实现细节。
+
+            缺点：
+                增加了系统的复杂度
+             
+             */
+
+        }
+    }
+
+    #region 遥控器
+
+    /// <summary>
+    /// 抽象概念中的遥控器，扮演抽象化角色
+    /// </summary>
+    public class RemoteControl
+    {
+        // 字段
+        private TV implementor;
+
+        // 属性
+        public TV Implementor
+        {
+            get { return implementor; }
+            set { implementor = value; }
+        }
+
+        /// <summary>
+        /// 开电视机，这里抽象类中不再提供实现了，而是调用实现类中的实现
+        /// </summary>
+        public virtual void On()
+        {
+            implementor.On();
+        }
+
+        /// <summary>
+        /// 关电视机
+        /// </summary>
+        public virtual void Off()
+        {
+            implementor.Off();
+        }
+
+        /// <summary>
+        /// 换频道
+        /// </summary>
+        public virtual void SetChannel()
+        {
+            implementor.tuneChannel();
+        }
+    }
+
+    /// <summary>
+    /// 具体遥控器
+    /// </summary>
+    public class ConcreteRemote : RemoteControl
+    {
+        public override void SetChannel()
+        {
+            Console.WriteLine("---------------------");
+            base.SetChannel();
+            Console.WriteLine("---------------------");
+        }
+    }
+
+    #endregion
+
+    #region 电视机
+
+    /// <summary>
+    /// 电视机，提供抽象方法
+    /// </summary>
+    public abstract class TV
+    {
+        /// <summary>
+        /// 开
+        /// </summary>
+        public abstract void On();
+
+        /// <summary>
+        /// 关
+        /// </summary>
+        public abstract void Off();
+
+        /// <summary>
+        /// 换频道
+        /// </summary>
+        public abstract void tuneChannel();
+    }
+
+    /// <summary>
+    /// 长虹牌电视机，重写基类的抽象方法
+    /// 提供具体的实现
+    /// </summary>
+    public class ChangHong : TV
+    {
+        public override void On()
+        {
+            Console.WriteLine("长虹牌电视机已经打开了");
+        }
+
+        public override void Off()
+        {
+            Console.WriteLine("长虹牌电视机已经关掉了");
+        }
+
+        public override void tuneChannel()
+        {
+            Console.WriteLine("长虹牌电视机换频道");
+        }
+    }
+
+    /// <summary>
+    /// 三星牌电视机，重写基类的抽象方法
+    /// </summary>
+    public class Samsung : TV
+    {
+        public override void On()
+        {
+            Console.WriteLine("三星牌电视机已经打开了");
+        }
+
+        public override void Off()
+        {
+            Console.WriteLine("三星牌电视机已经关掉了");
+        }
+
+        public override void tuneChannel()
+        {
+            Console.WriteLine("三星牌电视机换频道");
+        }
+    }
+
+    #endregion
+}
---
+++ 8.桥接模式/Program2.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.桥接模式
+{
+    internal class Program2
+    {
+        // 桥接模式 ------ 三层架构中的应用
+        // 客户端调用
+        // 类似Web应用程序
+        private static void Main(string[] args)
+        {
+            BaseBLL customers = new BLL("ShangHai");
+            customers.dal = new DAL();
+
+            customers.Add("小六");
+
+            customers.Delete("王五");
+
+            customers.Update("Learning_Hard");
+
+            customers.ShowAll();
+
+            Console.Read();
+        }
+    }
+
+    #region BLL
+
+    public class BaseBLL
+    {
+        private string city;
+
+        public BaseBLL(string city)
+        {
+            this.city = city;
+        }
+
+        // 属性
+        public BaseDAL dal { get; set; }
+
+        /// <summary>
+        /// 增加
+        /// </summary>
+        /// <param name="name"></param>
+        public virtual void Add(string name)
+        {
+            dal.AddRecord(name);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="name"></param>
+        public virtual void Delete(string name)
+        {
+            dal.DeleteRecord(name);
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="name"></param>
+        public virtual void Update(string name)
+        {
+            dal.UpdateRecord(name);
+        }
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public virtual string Get(int index)
+        {
+            return dal.GetRecord(index);
+        }
+
+        /// <summary>
+        /// 显示所有
+        /// </summary>
+        public virtual void ShowAll()
+        {
+            Console.WriteLine();
+            Console.WriteLine("{0}的顾客有：", city);
+            dal.ShowAllRecords();
+        }
+    }
+
+    public class BLL : BaseBLL
+    {
+        public BLL(string city)
+            : base(city) { }
+
+        // 重写方法
+        public override void ShowAll()
+        {
+            Console.WriteLine("------------------------");
+            base.ShowAll();
+            Console.WriteLine("------------------------");
+        }
+    }
+
+    #endregion BLL
+
+    #region DAL
+
+    public abstract class BaseDAL
+    {
+        /// <summary>
+        /// 增加
+        /// </summary>
+        /// <param name="name"></param>
+        public abstract void AddRecord(string name);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="name"></param>
+        public abstract void DeleteRecord(string name);
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="name"></param>
+        public abstract void UpdateRecord(string name);
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public abstract string GetRecord(int index);
+
+        /// <summary>
+        /// 显示所有
+        /// </summary>
+        public abstract void ShowAllRecords();
+    }
+
+    public class DAL : BaseDAL
+    {
+        // 字段
+        private List<string> customers = new List<string>();
+
+        public DAL()
+        {
+            // 实际业务中从数据库中读取数据再填充列表
+            customers.Add("Learning Hard");
+            customers.Add("张三");
+            customers.Add("李四");
+            customers.Add("王五");
+        }
+
+        // 重写方法
+        public override void AddRecord(string name)
+        {
+            customers.Add(name);
+        }
+
+        public override void DeleteRecord(string name)
+        {
+            customers.Remove(name);
+        }
+
+        public override void UpdateRecord(string updatename)
+        {
+            customers[0] = updatename;
+        }
+
+        public override string GetRecord(int index)
+        {
+            return customers[index];
+        }
+
+        public override void ShowAllRecords()
+        {
+            foreach (string name in customers)
+            {
+                Console.WriteLine(" " + name);
+            }
+        }
+    }
+
+    #endregion DAL
+}
